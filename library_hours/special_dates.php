b866a2d8aa1cfe0147569c2ee15b8301cb1107fe
studiozut <studiozut@gmail.com>
2016-12-19 14:16:50 -0500

    added special dates file

---
+++ library_hours/special_dates.php
@@ -0,0 +1,33 @@
+<?php
+// set $special =  'yes' to override hours with these values. Module logic checks against up to 4 special dates below. Set unused dates to something that won't match, like $spec4day = '40'.
+$special = 'yes';
+
+$spec1day = '22';
+$spec1dayname = 'Thursday';
+$spec1month = '12';
+$spec1open = '7:11AM';
+$spec1close = '10:11PM';
+$spec1string = 'closing at 10:00PM TEST';
+
+$spec2day = '40';
+$spec2dayname = 'Thursday';
+$spec2month = '12';
+$spec2open = '12:00AM';
+$spec2close = '10:11PM';
+$spec2string = 'spec 2 (20th)';
+
+$spec3day = '40';
+$spec3dayname = 'Thursday';
+$spec3month = '12';
+$spec3open = '12:00AM';
+$spec3close = '10:11PM';
+$spec3string = 'spec 3 (19th)';
+
+$spec4day = '40';
+$spec4dayname = 'Thursday';
+$spec4month = '12';
+$spec4open = '12:00AM';
+$spec4close = '10:00PM';
+$spec4string = 'spec 4 (25th)';
+
+?>
